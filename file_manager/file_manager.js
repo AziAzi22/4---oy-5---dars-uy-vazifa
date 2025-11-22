02c3ba8a9099a3f24edd6d30c2830bea6efbeaa0
AziAzi22 <aziazi22t@gmail.com>
2025-11-23 01:54:08 +0500

    ff

---
+++ file_manager/file_manager.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const read_file = file_name => {
+return JSON.parse(fs.readFileSync("./data/" + file_name, "utf-8"))
+}
+const write_file = (file_name, data) => {
+    fs.writeFileSync("./data/" + file_name, JSON.stringify(data, null, 4))
+}
+
+module.exports = {
+    read_file,
+    write_file
+}
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const fs = require("fs");
 const uuid = require("uuid");
+const http = require("http");
+const option = {
+  "content-type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
+
+const app = http.createServer((req, res) => {
+  const reqID = req.url.split("/").pop();
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
